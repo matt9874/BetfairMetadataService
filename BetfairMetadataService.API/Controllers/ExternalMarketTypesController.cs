df8ec0db610877b98b099a1585ac1ef76be263cf
matt9874 <mattcave623@gmail.com>
2020-05-08 21:01:36 +0100

    Add handling of NonExistent entities in the ExternalMarketTypesController

--- BetfairMetadataService.API/Controllers/ExternalMarketTypesController.cs
+++ BetfairMetadataService.API/Controllers/ExternalMarketTypesController.cs
@@ -13,11 +13,18 @@ namespace BetfairMetadataService.API.Controllers
     [ApiController]
     public class ExternalMarketTypesController : ControllerBase
     {
+        private readonly IReader<DataProvider, int> _dataProviderReader;
+        private readonly IReader<EventType, string> _eventTypeReader;
+        private readonly IReader<Competition, string> _competitionReader;
         private readonly Func<int, IMarketTypesService> _marketTypesServiceFactory;
         private readonly IMapper _mapper;
 
-        public ExternalMarketTypesController(Func<int, IMarketTypesService> marketTypesServiceFactory, IMapper mapper)
+        public ExternalMarketTypesController(IReader<DataProvider, int> dataProviderReader, IReader<EventType, string> eventTypeReader,
+            IReader<Competition, string> competitionReader, Func<int, IMarketTypesService> marketTypesServiceFactory, IMapper mapper)
         {
+            _dataProviderReader = dataProviderReader;
+            _eventTypeReader = eventTypeReader;
+            _competitionReader = competitionReader;
             _marketTypesServiceFactory = marketTypesServiceFactory;
             _mapper = mapper;
         }
@@ -25,6 +32,14 @@ namespace BetfairMetadataService.API.Controllers
         [HttpGet("dataProviders/{dataProviderId}/competitions/{competitionId}/marketTypes")]
         public async Task<IActionResult> GetMarketTypesByCompetition(int dataProviderId, string competitionId)
         {
+            DataProvider dataProvider = await _dataProviderReader.Read(dataProviderId);
+            if (dataProvider == null)
+                return NotFound($"Unable to find dataProvider with id {dataProviderId}");
+
+            Competition competition = await _competitionReader.Read(competitionId);
+            if (competition == null)
+                return NotFound($"Unable to find competition with id {competitionId}");
+
             IMarketTypesService marketTypesService = _marketTypesServiceFactory?.Invoke(dataProviderId);
             IEnumerable<MarketType> marketTypes = await marketTypesService.GetMarketTypesByCompetitionId(competitionId);
 
@@ -38,6 +53,14 @@ namespace BetfairMetadataService.API.Controllers
         [HttpGet("dataProviders/{dataProviderId}/eventTypes/{eventTypeId}/marketTypes")]
         public async Task<IActionResult> GetMarketTypesByEventType(int dataProviderId, string eventTypeId)
         {
+            DataProvider dataProvider = await _dataProviderReader.Read(dataProviderId);
+            if (dataProvider == null)
+                return NotFound($"Unable to find dataProvider with id {dataProviderId}");
+
+            EventType eventType = await _eventTypeReader.Read(eventTypeId);
+            if (eventType == null)
+                return NotFound($"Unable to find eventType with id {eventTypeId}");
+
             IMarketTypesService marketTypesService = _marketTypesServiceFactory?.Invoke(dataProviderId);
             IEnumerable<MarketType> marketTypes = await marketTypesService.GetMarketTypesByEventTypeId(eventTypeId);
 
